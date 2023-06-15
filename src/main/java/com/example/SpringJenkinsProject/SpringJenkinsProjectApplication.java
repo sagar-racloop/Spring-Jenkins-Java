106ceec280d856ec018fd9a9a8ce462569d9dd05
sagar-racloop <sagar@racloop.com>
2023-06-15 13:29:09 +0530

    Finally Done

---
+++ src/main/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplication.java
@@ -0,0 +1,29 @@
+package com.example.SpringJenkinsProject;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsProjectApplication.class);
+	@PostConstruct
+	public void intt(){
+		logger.info("Application Started ...");
+	}
+
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+		logger.info("Application executed ...");
+
+
+	}
+
+}
---
+++ src/test/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.SpringJenkinsProject;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.logging.Logger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsProjectApplicationTests {
+	public static Logger logger= (Logger) LoggerFactory.getLogger(SpringJenkinsProjectApplication.class);
+
+
+	@Test
+	void contextLoads() {
+
+		logger.info("Test Case Executing");
+
+		assertEquals(true,true);
+	}
+
+}
