7ee9a130f1cf2cc6a79c655ce3ed64c67118d20c
sagar-racloop <sagar@racloop.com>
2023-06-15 14:22:37 +0530

    Done

--- src/main/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplication.java
+++ src/main/java/com/example/SpringJenkinsProject/SpringJenkinsProjectApplication.java
@@ -1,15 +1,19 @@
 package com.example.SpringJenkinsProject;
 
-import jakarta.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class SpringJenkinsProjectApplication {
 
 	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsProjectApplication.class);
+
+
 	@PostConstruct
 	public void intt(){
 		logger.info("Application Started ...");
